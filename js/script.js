c1afc536ee01a53e1583e054f66b68d131b30fb7
dasyx <seanpnn00@gmail.com>
2021-10-15 01:05:35 +0200

    work on seo + lighthouse

--- js/script.js
+++ js/script.js
@@ -24,3 +24,24 @@ overlay.addEventListener('mouseout', function () {
     figcap_text.style.display = "none";
     figcap_text2.style.display = "none";
 })
+
+// Gestion de l'effet ripple au passage sur les boutons
+
+window.onload = function() {
+    let rippleElements = document.getElementsByClassName("myRipple");
+    for(var i = 0; i < rippleElements.length; i++) {
+        rippleElements[i].onclick = function(e) {
+          let X = e.pageX - this.offsetLeft;
+          let Y = e.pageY - this.offsetTop;
+          let rippleDiv = document.createElement("div");
+          rippleDiv.classList.add('ripple');
+          rippleDiv.setAttribute("style","top:"+Y+"px; left:"+X+"px;");
+          let customColor = this.getAttribute('ripple-color');
+          if(customColor) rippleDiv.style.background = customColor;
+          this.appendChild(rippleDiv);
+          setTimeout(function(){
+            rippleDiv.parentElement.removeChild(rippleDiv);
+          }, 900);
+    }
+  }
+  }
