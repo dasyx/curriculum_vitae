e4a33387c710a2a0dc9c77e3fb24460c63fb74b0
dasyx <seanpnn00@gmail.com>
2021-10-18 02:15:40 +0200

    portfolio js anime

--- js/script.js
+++ js/script.js
@@ -1,43 +1,54 @@
-
 // Ecoute de l'événement passage de souris pour afficher l'overlay sur les images du portfolio
 
 let overlay = document.getElementById('mouse_out');
 let displayTop = document.getElementsByClassName('overlay_top');
 let displayBottom = document.getElementsByClassName('overlay_bottom');
-let figcap = document.getElementById('text');
-let figcap2 = document.getElementById('text2');
-let figcap_text = document.getElementById('text_icon');
-let figcap_text2 = document.getElementById('text_icon2');
+let figcap_top_sec = document.getElementsByClassName('text_top_second');
+let figcap_bottom = document.getElementsByClassName('text_bottom');
+let figcap_top = document.getElementsByClassName('text_top');
+let figcap_icon2 = document.getElementsByClassName('text_icon2');
+let arrow = document.getElementsByClassName('bi');
+let icon_display = document.getElementsByClassName('icon_display');
+let icon = document.createElement('i');
 
 overlay.addEventListener('mouseover', function () {
-  figcap.textContent = "text de test";
-  figcap2.textContent = "text de test2";
-  figcap.style.display = "inline";
-  figcap2.style.display = "inline";
-  figcap_text.style.display = "inline";
-  figcap_text2.style.display = "inline";
 
-  for (i = 0; i < displayBottom.length; i++) {
+  for (i = 0; i < displayBottom.length && displayTop.length; i++) {
     displayBottom[i].style.width = "420px";
     displayBottom[i].style.height = "200px";
-  }
-  for (i = 0; i < displayTop.length; i++) {
     displayTop[i].style.width = "420px";
     displayTop[i].style.height = "200px";
   }
+  for (i = 0; i < figcap_top_sec.length; i++) {
+    figcap_top_sec[i].style.display = "inline";
+    figcap_top_sec[i].textContent = "text de test";
+  }
+  for (i = 0; i < figcap_bottom.length; i++) {
+    figcap_bottom[i].style.display = "inline";
+    figcap_bottom[i].textContent = "text de test2";
+  }
+  for (i = 0; i < figcap_top.length && figcap_icon2.length; i++) {
+    figcap_top[i].style.display = "inline";
+    figcap_icon2[i].style.display = "inline";
+  }
+  for (i = 0; i < icon_display.length; i++) {
+    icon.setAttribute("class", "bi bi-arrow-right-circle");
+    icon_display[i].appendChild(icon);
+    for (i = 0; i < arrow.length; i++) {
+      arrow[i].style.display = "block";
+      arrow[i].style.textAlign = "center";
+    }
+  }
+  /* if (figcap[0]) {
+    figcap[0].innerHTML = "ohmyfood";
+  } */
 })
 
 overlay.addEventListener('mouseout', function () {
-  figcap.textContent = "";
-  figcap2.textContent = "";
-  figcap_text.textContent = "";
-  figcap_text.textContent = "";
 
-  for (i = 0; i < displayBottom.length; i++) {
+  for (i = 0; i < displayBottom.length && displayTop; i++) {
     displayBottom[i].style.width = "0px";
     displayBottom[i].style.height = "0px";
-  }
-  for (i = 0; i < displayTop.length; i++) {
     displayTop[i].style.width = "0px";
     displayTop[i].style.height = "0px";
   }
