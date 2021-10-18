64c765ec6d690b80562a6e6e2399edbca7f44a8b
dasyx <seanpnn00@gmail.com>
2021-10-19 01:23:15 +0200

    portfolio js overlay display + icons

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@
 let overlay = document.getElementById('mouse_out');
 let displayTop = document.getElementsByClassName('overlay_top');
 let displayBottom = document.getElementsByClassName('overlay_bottom');
+let figcap_top = document.getElementsByClassName('text_top');
 let figcap_top_sec = document.getElementsByClassName('text_top_second');
 let figcap_bottom = document.getElementsByClassName('text_bottom');
-let figcap_top = document.getElementsByClassName('text_top');
 let figcap_icon2 = document.getElementsByClassName('text_icon2');
-let arrow = document.getElementsByClassName('bi');
-let icon_display = document.getElementsByClassName('icon_display');
+let figcap_tech1 = document.getElementsByClassName('text_techno1');
+let figcap_tech2 = document.getElementsByClassName('text_techno2');
+let figcap_tech3 = document.getElementsByClassName('text_techno3');
+let figcap_top_icon = document.getElementsByClassName('bi');
+let icon_top_display = document.getElementsByClassName('icon_top_display');
+let icon_bottom_display = document.getElementsByClassName('icon_bottom_display');
 let icon = document.createElement('i');
 
 overlay.addEventListener('mouseover', function () {
@@ -19,28 +23,50 @@ overlay.addEventListener('mouseover', function () {
     displayTop[i].style.width = "420px";
     displayTop[i].style.height = "200px";
   }
-  for (i = 0; i < figcap_top_sec.length; i++) {
-    figcap_top_sec[i].style.display = "inline";
-    figcap_top_sec[i].textContent = "text de test";
+  for (i = 0; i < figcap_icon2.length && figcap_bottom.length; i++) {
+    figcap_icon2[i].style.display = "inline";
+    figcap_bottom[i].style.display = "inline";
+  }
+  for (i = 0; i < figcap_top_sec.length && figcap_top; i++) {
+    figcap_top_sec[i].textContent = "Voir le site";
+    figcap_top[i].style.textAlign = "center";
+    figcap_top_sec[i].style.margi = "center";
+  }
+  for (i = 0; i < figcap_top.length && figcap_top_sec.length; i++) {
+    figcap_top[i].style.display = "flex";
+    figcap_top_sec[i].style.display = "flex";
   }
   for (i = 0; i < figcap_bottom.length; i++) {
-    figcap_bottom[i].style.display = "inline";
-    figcap_bottom[i].textContent = "text de test2";
+    figcap_bottom[i].textContent = "Voir le code";
   }
-  for (i = 0; i < figcap_top.length && figcap_icon2.length; i++) {
-    figcap_top[i].style.display = "inline";
-    figcap_icon2[i].style.display = "inline";
+  // Affichage conditionnel spécifique du contenu portofilio pour chaque projet
+  if (figcap_top[0]) {
+    figcap_top[0].innerHTML = "Dynamiser une page web avec des animations CSS";
+    figcap_tech1[0].innerHTML = "HTML";
+    figcap_tech1[0].style.margin = "10px";
+    figcap_tech2[0].innerHTML = "CSS";
+    figcap_tech2[0].style.margin = "10px";
+    figcap_tech3[0].innerHTML = "SASS";
+    figcap_tech3[0].style.margin = "10px";
   }
-  for (i = 0; i < icon_display.length; i++) {
+  if (figcap_top[1]) {
+    figcap_top[1].innerHTML = "Optimiser un site web existant";
+  }
+  if (figcap_top[2]) {
+    figcap_top[2].innerHTML = "Construire un site e-commerce";
+  }
+  if (figcap_top[3]) {
+    figcap_top[3].innerHTML = "Construire une API sécurisée pour une application d'avis gastronomiques";
+  }
+  if (figcap_top[4]) {
+    figcap_top[4].innerHTML = "Créer un réseau social d'entrprise";
+  }
+  if (figcap_top[5]) {
+    figcap_top[5].innerHTML = "Transformer une maquette en site web";
+  }
+  /* for (i = 0; i < icon_display.length; i++) {
     icon.setAttribute("class", "bi bi-arrow-right-circle");
-    icon_display[i].appendChild(icon);
-    for (i = 0; i < arrow.length; i++) {
-      arrow[i].style.display = "block";
-      arrow[i].style.textAlign = "center";
-    }
-  }
-  /* if (figcap[0]) {
-    figcap[0].innerHTML = "ohmyfood";
+    icon_display[i].append(icon);
   } */
 })
 
